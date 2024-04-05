fad751046dfccb51d827f6c430b8fd3d43ee8b62
DuduGuima <eduardo.guima.dudu@gmail.com>
2024-04-05 17:41:08 +0200

    lab 5 init

--- 00std_speaker_1_cpu/00std_speaker_1_cpu.c
+++ 00std_speaker_1_cpu/00std_speaker_1_cpu.c
@@ -1,19 +1,30 @@
 #include "nrf52833.h"
 
-void wait(void) {
+
+#define max_its 200
+//s
+
+void wait(int reps) {
     volatile uint32_t a;
-    for (a=0; a<8000;a++);
+    for (a=0; a<reps;a++);
 }
 
 int main(void) {
-    
+    // loop variables
+    int i,it = 0;
     // set speaker pin as output
     NRF_P0->PIN_CNF[0] = 0x00000003; // P0.00
 
     while(1) {
-        NRF_P0->OUTSET = (0x00000001 << 0); // speaker pin high
-        wait();
-        NRF_P0->OUTCLR = (0x00000001 << 0); // speaker pin low
-        wait();
+        if (it == max_its) break;
+        for (i=1;i<2000;i++){
+            NRF_P0->OUTSET = (0x00000001 << 0); // speaker pin high
+            wait(i);
+            NRF_P0->OUTCLR = (0x00000001 << 0); // speaker pin low
+            wait(i);
+        }
+        ++it;
     }
+
+    
 }
